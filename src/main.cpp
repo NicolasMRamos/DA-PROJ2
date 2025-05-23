0b4a8aaf1b49525aabe2c79799cc7135881bc995
NicolasMRamos <nicolasmagalhaesramos19@gmail.com>
2025-05-24 00:42:19 +0100

    Modularized menu; Added doxygen

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,11 @@
  * Brute Force, Greedy, Dynamic Programming and ILP.
  */
 
-#include <iostream>
-
 #include "menu.h"
-#include "parsing.h"
-#include "greedy.h"
-#include "backtracking.h"
-#include "dynamicprog.h"
 
 int main(){
   bool approach_selected = false, dataset_loaded = false;
-  int approach, option, dataset;
+  int approach = 0, option, dataset;
   std::string number, filenameTruck, filenamePallets;
   std::pair<int, int> capPal;
   std::vector<Pallet> pallets;
@@ -33,102 +27,21 @@ int main(){
       std::cout << "Insert dataset id:" << std::endl;
       std::cin >> dataset;
 
-      if (dataset < 1 || dataset > 10) {
-        std::cout << std::endl;
-        std::cout << "Invalid dataset id. Try again." << std::endl;
-      } else {
-        if (dataset < 10) {
-          number = '0' + std::to_string(dataset);
-        }
-        else if (dataset == 10) {
-          number = std::to_string(dataset);
-        }
-
-        filenameTruck = "../data/TruckAndPallets_" + number + ".csv";
-        filenamePallets = "../data/Pallets_" + number + ".csv";
-
-        capPal = parseTruck(filenameTruck);
-        pallets = parsePallets(filenamePallets, capPal.second);
-
-        dataset_loaded = true;
-      }
+      loadDataset(dataset, filenameTruck, filenamePallets, number, capPal, pallets, dataset_loaded);
 
     } else if (option == 2) {
 
       approach = approaches();
-      approach_selected = approach;
+
+      approach_selected = true;
 
     } else if (option == 3) {
 
-      if (dataset_loaded) {
-        std::cout << std::endl;
-        std::cout << "Loaded truck:" << std::endl;
-        std::cout << "TruckAndPallets_" + number + ".csv" << std::endl;
-        std::cout << "Loaded pallets:" << std::endl;
-        std::cout << "Pallets_" + number + ".csv" << std::endl;
-      } else {
-        std::cout << std::endl;
-        std::cout << "No dataset loaded." << std::endl;
-      }
-
-      if (approach_selected) {
-        std::cout << std::endl;
-        std::cout << "Approach selected:" << std::endl;
-        if (approach == 1) {
-          std::cout << "Brute Force + Backtracking" << std::endl;
-        } else if (approach == 2) {
-          std::cout << "Greedy" << std::endl;
-        } else if (approach == 3) {
-          std::cout << "Dynamic Programming" << std::endl;
-        } else if (approach == 4) {
-          std::cout << "ILP" << std::endl;
-        } else {
-          std::cout << "Invalid approach selected. Try again." << std::endl;
-          approach_selected = false;
-        }
-      } else {
-        std::cout << std::endl;
-        std::cout << "No approach selected." << std::endl;
-      }
+      showSettings(number, dataset_loaded, approach_selected, approach);
 
     } else if (option == 4) {
 
-      if (approach_selected && dataset_loaded) {
-
-        std::pair<int, std::vector<int>> result;
-
-        /*
-        for (auto i:pallets) {
-          std::cout << "Pallet: " << i.id << ' ' << i.weight << ' ' << i.profit << '\n';
-        }
-
-        std::cout << "Truck: " << capPal.first << ' ' << capPal.second << '\n';
-        */
-
-        if (approach == 1) {
-          result = backtrackingApproach(pallets, capPal.first);
-        } else if (approach == 2) {
-          result = greedyApproach(pallets, capPal.first);
-        } else if (approach == 3) {
-          result = dpApproach(pallets, capPal.first);
-        } else if (approach == 4) {
-          std::cout << std::endl;
-          std::cout << "ILP placeholder" << std::endl;
-        }
-
-        std::cout << std::endl;
-        std::cout << "Total profit: " << result.first << '\n';
-        std::cout << "Pallets: ";
-        for (auto i:result.second)
-        {
-          std::cout << i << ' ';
-        }
-        std::cout << '\n';
-
-      } else {
-        std::cout << std::endl;
-        std::cout << "No dataset loaded and/or approach selected. Try again after loading a dataset and selecting an approach." << std::endl;
-      }
+      run(approach, capPal, pallets, approach_selected, dataset_loaded);
 
     } else if (option == 5) {
 
--- src/other/menu.cpp
+++ src/other/menu.cpp
@@ -31,3 +31,98 @@ int approaches(){
     return number;
 }
 
+void loadDataset(const int dataset, std::string &filenameTruck, std::string &filenamePallets, std::string &number, std::pair<int, int> &capPal, std::vector<Pallet> &pallets, bool &dataset_loaded) {
+
+    if (dataset < 1 || dataset > 10) {
+        std::cout << std::endl;
+        std::cout << "Invalid dataset id. Try again." << std::endl;
+    } else {
+        if (dataset < 10) {
+            number = '0' + std::to_string(dataset);
+        }
+        else if (dataset == 10) {
+            number = std::to_string(dataset);
+        }
+
+        filenameTruck = "../data/TruckAndPallets_" + number + ".csv";
+        filenamePallets = "../data/Pallets_" + number + ".csv";
+
+        capPal = parseTruck(filenameTruck);
+        pallets = parsePallets(filenamePallets, capPal.second);
+
+        dataset_loaded = true;
+    }
+}
+
+void showSettings(const std::string& number, const bool dataset_loaded, bool &approach_selected, const int approach) {
+
+    if (dataset_loaded) {
+        std::cout << std::endl;
+        std::cout << "Loaded truck:" << std::endl;
+        std::cout << "TruckAndPallets_" + number + ".csv" << std::endl;
+        std::cout << "Loaded pallets:" << std::endl;
+        std::cout << "Pallets_" + number + ".csv" << std::endl;
+    } else {
+        std::cout << std::endl;
+        std::cout << "No dataset loaded." << std::endl;
+    }
+
+    if (approach_selected) {
+        std::cout << std::endl;
+        std::cout << "Approach selected:" << std::endl;
+        if (approach == 1) {
+            std::cout << "Brute Force + Backtracking" << std::endl;
+        } else if (approach == 2) {
+            std::cout << "Greedy" << std::endl;
+        } else if (approach == 3) {
+            std::cout << "Dynamic Programming" << std::endl;
+        } else if (approach == 4) {
+            std::cout << "ILP" << std::endl;
+        } else {
+            std::cout << "Invalid approach selected. Try again." << std::endl;
+            approach_selected = false;
+        }
+    } else {
+        std::cout << std::endl;
+        std::cout << "No approach selected." << std::endl;
+    }
+}
+
+void run(const int approach, const std::pair<int, int> &capPal, std::vector<Pallet> pallets, const bool approach_selected, const bool dataset_loaded) {
+
+    if (approach_selected && dataset_loaded) {
+
+        std::pair<int, std::vector<int>> result;
+
+        /*
+        for (auto i:pallets) {
+          std::cout << "Pallet: " << i.id << ' ' << i.weight << ' ' << i.profit << '\n';
+        }
+
+        std::cout << "Truck: " << capPal.first << ' ' << capPal.second << '\n';
+        */
+
+        if (approach == 1) {
+            result = backtrackingApproach(pallets, capPal.first);
+        } else if (approach == 2) {
+            result = greedyApproach(pallets, capPal.first);
+        } else if (approach == 3) {
+            result = dpApproach(pallets, capPal.first);
+        } else if (approach == 4) {
+            std::cout << std::endl;
+            std::cout << "ILP placeholder" << std::endl;
+        }
+
+        std::cout << std::endl;
+        std::cout << "Total profit: " << result.first << '\n';
+        std::cout << "Pallets: ";
+        for (const auto i:result.second) {
+            std::cout << i << ' ';
+        }
+        std::cout << '\n';
+
+    } else {
+        std::cout << std::endl;
+        std::cout << "No dataset loaded and/or approach selected. Try again after loading a dataset and selecting an approach." << std::endl;
+    }
+}
